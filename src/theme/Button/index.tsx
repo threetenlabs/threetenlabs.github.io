92290db4f10b9cf72ae65d13eb7cea2df2f87d66
Ryan Sites <ryan.r.sites@gmail.com>
2023-11-24 16:28:42 -0500

    add buttons

---
+++ src/theme/Button/index.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react';
+
+import './button.css';
+
+type ButtonProps = {
+  theme?: 'dark' | 'light';
+  height?: number;
+  width?: number;
+  logo: string;
+  storeName: string;
+  title: string;
+  url: string;
+  className?: string;
+  border?: number;
+};
+
+const Button: FC<ButtonProps> = ({
+  theme = 'light',
+  height = 50,
+  width = 180,
+  border,
+  logo,
+  storeName,
+  title,
+  url,
+  className,
+}) => {
+  return (
+    <div
+      onClick={() => url && window.open(url, '_blank')}
+      style={{
+        height: height,
+        width: width,
+        borderRadius: border,
+      }}
+      className={`button-container button-container-${theme} ${className}`}>
+      <img src={logo} alt={storeName} />
+      <div className="button-text-container">
+        <span className="button-title">{title}</span>
+        <span className="button-store-name">{storeName}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/theme/GooglePlayButton/index.tsx
@@ -0,0 +1,73 @@
+import React, { FC } from 'react';
+
+// import GooglePlay from '';
+import Button from '../Button';
+
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+type GooglePlayButtonProps = {
+  /**
+   * @default dark
+   */
+  theme?: 'dark' | 'light';
+
+  /**
+   * @default GET IT ON
+   */
+  title?: string;
+
+  /**
+   * @default 60px
+   */
+  height?: number;
+
+  /**
+   * @default 180px
+   */
+  width?: number;
+
+  /**
+   * @default ''
+   */
+  className?: string;
+
+  /**
+   * @default ''
+   */
+  url: string;
+};
+
+/**
+ *
+ * @param {string} theme Choose a theme between dark and light
+ * @param {number} height Controls the height of the button
+ * @param {number} width Controls the width of the button
+ * @param {string} title Set custom title for the button
+ * @param {string} className Add className to the button in order to customize the button appearance
+ * @param {string} url Add your store url to the button
+ * @example <GooglePlayButton theme="light" height={70} width={130} className="button-container" url="https://play.google.com/" />
+ * @returns
+ */
+const GooglePlayButton: FC<GooglePlayButtonProps> = ({
+  theme = 'light',
+  height,
+  title = 'GET IT ON',
+  width,
+  className,
+  url,
+}) => {
+  return (
+    <Button
+      theme={theme}
+      height={height}
+      width={width}
+      url={url}
+      storeName={'Google Play'}
+      logo={useBaseUrl('/img/Google-Play.svg')}
+      className={className}
+      title={title}
+    />
+  );
+};
+
+export default GooglePlayButton;
